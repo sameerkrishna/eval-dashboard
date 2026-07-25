8d54fa50d4d354d75808fdd520ae687d02dc5bd3
Bulusu Sai Sameer Krishna <bsk11@iitbbs.ac.in>
2026-07-25 18:26:37 +0530

    Updated PortfolioDashboard.tsx

--- src/screens/PortfolioDashboard.tsx
+++ src/screens/PortfolioDashboard.tsx
@@ -271,7 +271,7 @@ export default function PortfolioDashboard() {
                           to={`/agents/${agent.agent_id}`}
                           className="text-lg text-primary-600 hover:text-primary-800 dark:text-primary-400 dark:hover:text-primary-300 font-bold"
                         >
-                           &gt;
+                           Read More&gt;
                         </Link>
                       </td>
                     </tr>
